316fcc9f3712157834baef550350a46aeefca9de
dayet <296577630@qq.com>
2019-09-09 22:53:41 +0800

    init repo

---
+++ src/main/java/cn/com/dayet/DayetApplication.java
@@ -0,0 +1,13 @@
+package cn.com.dayet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayetApplication.class, args);
+    }
+
+}
